ad91a51926c8b2775bd7c68bd610bd2d6b22bd6d
ayrocout <49459986+ayrocout@users.noreply.github.com>
2019-04-14 17:14:53 -0300

    novos arquivos atualizados

---
+++ funcao.js
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+
+c = a+=b;
+
+console.log("o valor da variavel c é ",c);
+
+
+//FIXME: a funcao nao retorna cliente
+function retornoCliente(){
+    console.log('cliente indefinido');
+}
